b9f5f8afd8c7fefb5e42c83c0820c51eb5310a08
MAHABIR PAUL <156224587+mahabir163@users.noreply.github.com>
2026-08-12 19:33:07 +0530

    Add WSGI application setup in settings.py

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = get_wsgi_application()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
